d0f6a02977ffdf98d479d32ec6e082e20518631e
julp <julp@redaction-developpez.com>
2011-12-27 21:23:58 +0100

    add first imp of stpcpy_s

---
+++ string/stpcpy_s.c
@@ -0,0 +1,16 @@
+char *stpcpy_s(char *to, const char *from, const char * const zero)
+{
+    const char * const end = zero - 1;
+
+    if (NULL == to || to >= end) {
+        return NULL;
+    }
+    while (to < end && 0 != (*to++ = *from++))
+        ;
+    if (to == end) {
+        *to = 0;
+        return NULL;
+    } else {
+        return to - 1;
+    }
+}
