cc76fa7a3dad8e6c3fc6cb33401248b2814ecb4a
julp <poinsot.julien@gmail.com>
2012-12-31 18:59:24 +0100

    ascii case insenstive functions

--- string/strcmp_l.c
+++ string/strcmp_l.c
@@ -4,17 +4,27 @@ int strcmp_l(
 ) {
     const char *end;
 
-    if (str2_len > str1_len) {
-        end = str2 + str2_len;
-    } else {
-        end = str1 + str1_len;
-    }
-    while (str1 <= end) {
-        if (*str1 != *str2) {
-            return (unsigned char) *str1 - (unsigned char) *str2;
+    if (str1 != str2) {
+        if (str2_len > str1_len) {
+            end = str2 + str2_len;
+        } else {
+            end = str1 + str1_len;
         }
-        ++str1, ++str2;
+        while (str1 <= end) {
+            if (*str1 != *str2) {
+                return (unsigned char) *str1 - (unsigned char) *str2;
+            }
+            ++str1, ++str2;
+        }
     }
 
     return str1_len - str2_len;
+}
+
+int strncmp_l(
+    const char *str1, int32_t str1_len,
+    const char *str2, int32_t str2_len,
+    int n
+) {
+    //
 }
