ee93f2bd8689fc899d117b4ceaeaf09975e6b5f0
julp <poinsot.julien@gmail.com>
2014-09-08 18:15:57 +0200

    add few more tests

--- tests/parsenum-ut.h
+++ tests/parsenum-ut.h
@@ -16,13 +16,19 @@ UT(empty_string, int8_t, "", 0, PARSE_NUM_ERR_NO_DIGIT_FOUND, 0, 0, 0, -1);
 UT(empty_string, uint8_t, "", 0, PARSE_NUM_ERR_NO_DIGIT_FOUND, 0, 0, 0, -1);
 
 UT(just_unary_plus, int8_t, "+", 0, PARSE_NUM_ERR_NO_DIGIT_FOUND, 0, 0, 0, -1);
+UT(just_unary_plus, uint8_t, "+", 0, PARSE_NUM_ERR_NO_DIGIT_FOUND, 0, 0, 0, -1);
 UT(just_unary_minus, int8_t, "-", 0, PARSE_NUM_ERR_NO_DIGIT_FOUND, 0, 0, 0, -1);
+UT(just_unary_minus, uint8_t, "-", 0, PARSE_NUM_ERR_NO_DIGIT_FOUND, 0, 0, 0, -1);
 
 UT(unary_plus_number, int8_t, "+123", 123, PARSE_NUM_NO_ERR, 0, 0, 0, -1);
+UT(unary_plus_number, uint8_t, "+123", 123, PARSE_NUM_NO_ERR, 0, 0, 0, -1);
 UT(unary_minus_number, int8_t, "-123", -123, PARSE_NUM_NO_ERR, 0, 0, 0, -1);
+UT(unary_minus_number, uint8_t, "-123", (uint8_t) -123, PARSE_NUM_NO_ERR, 0, 0, 0, -1);
 
 UT(just_0x_base16, int8_t, "0x", 0, PARSE_NUM_ERR_NO_DIGIT_FOUND, 0, 0, 0, -1);
+UT(just_0x_base16, uint8_t, "0x", 0, PARSE_NUM_ERR_NO_DIGIT_FOUND, 0, 0, 0, -1);
 UT(just_0x_base10, int8_t, "0x", 0, PARSE_NUM_ERR_NON_DIGIT_FOUND, 10, 0, 0, STR_LEN("0x") - 1);
+UT(just_0x_base10, uint8_t, "0x", 0, PARSE_NUM_ERR_NON_DIGIT_FOUND, 10, 0, 0, STR_LEN("0x") - 1);
 
 UT(fine_5, int8_t, "5", 5, PARSE_NUM_NO_ERR, 0, 0, 0, -1);
 UT(fine_5, uint8_t, "5", 5, PARSE_NUM_NO_ERR, 0, 0, 0, -1);
@@ -38,14 +44,20 @@ UT(user_too_small, uint8_t, "2", 2, PARSE_NUM_ERR_LESS_THAN_MIN, 0, 5, 0, -1);
 UT(invalid_number_on_first_char, int8_t, "j10", 0, PARSE_NUM_ERR_NON_DIGIT_FOUND, 0, 0, 0, STR_LEN("j") - 1);
 UT(invalid_number_on_first_char, uint8_t, "j10", 0, PARSE_NUM_ERR_NON_DIGIT_FOUND, 0, 0, 0, STR_LEN("j") - 1);
 
+UT(truncation_on_non_digit, int8_t, "28:19", 28, PARSE_NUM_ERR_NON_DIGIT_FOUND, 0, 0, 0, STR_LEN("28:") - 1);
+UT(truncation_on_non_digit, uint8_t, "28:19", 28, PARSE_NUM_ERR_NON_DIGIT_FOUND, 0, 0, 0, STR_LEN("28:") - 1);
+
 UT(valid_auto_number, int8_t, "5", 5, PARSE_NUM_NO_ERR, 0, 0, 0, -1);
+UT(valid_auto_number, uint8_t, "5", 5, PARSE_NUM_NO_ERR, 0, 0, 0, -1);
 UT(valid_auto_binary, int8_t, "0b1001", 9, PARSE_NUM_NO_ERR, 0, 0, 0, -1);
 UT(valid_auto_binary, uint8_t, "0b1001", 9, PARSE_NUM_NO_ERR, 0, 0, 0, -1);
 UT(invalid_auto_binary, int8_t, "0b1201", 1, PARSE_NUM_ERR_NON_DIGIT_FOUND, 0, 0, 0, STR_LEN("0b12") - 1);
 UT(invalid_auto_binary, uint8_t, "0b1201", 1, PARSE_NUM_ERR_NON_DIGIT_FOUND, 0, 0, 0, STR_LEN("0b12") - 1);
 
 UT(zero_in_front_as_decimal, int32_t, "0123456789", 123456789, PARSE_NUM_NO_ERR, 10, 0, 0, -1);
+UT(zero_in_front_as_decimal, uint32_t, "0123456789", 123456789, PARSE_NUM_NO_ERR, 10, 0, 0, -1);
 UT(zero_in_front_as_auto, int32_t, "0123456789", 01234567, PARSE_NUM_ERR_NON_DIGIT_FOUND, 0, 0, 0, STR_LEN("012345678") - 1);
+UT(zero_in_front_as_auto, uint32_t, "0123456789", 01234567, PARSE_NUM_ERR_NON_DIGIT_FOUND, 0, 0, 0, STR_LEN("012345678") - 1);
 
 
 
