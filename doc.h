1d9283d5ad894d2c8d8b55a3adc34ce88791e5a6
julp <e2c2be7ed0f2f336@gmail.com>
2022-09-15 23:46:40 +0200

    to generate doxygen documentation

---
+++ doc.h
@@ -0,0 +1,47 @@
+/**
+ * @file
+ * @mainpage
+ *
+ * Strings:
+ * <ul>
+ *  <li>\ref string/parsenum.c</li>
+ *  <li>\ref string/ascii_case.c</li>
+ *  <li>
+ *   Safe (out of bounds) string.h functions variants:
+ *   <ul>
+ *     <li>\ref string/strcpy_s.c</li>
+ *     <li>\ref string/strcat_s.c</li>
+ *     <li>\ref string/stpcpy_s.c</li>
+ *   </ul>
+ *   <ul>
+ *    <li>
+ *     Binary safe/non-`\0` terminated strings:
+ *     <ul>
+ *      <li>\ref string/memncmp_l.c</li>
+ *      <li>\ref string/memcmp_l.c</li>
+ *     </ul>
+ *    </li>
+ *   </ul>
+ *  </li>
+ * </ul>
+ *
+ * Data structures:
+ * <ul>
+ *  <li>\ref iterator/iterator.c</li>
+ *  <li>
+ *   Lists:
+ *   <ul>
+ *    <li>\ref lists/dlist.c</li>
+ *   </ul>
+ *  </li>
+ *  <li>\ref hashtable/hashtable.c</li>
+ *  <ul>
+ *   <li>
+ *    Dynamic arrays:
+ *    <ul>
+ *     <li>\ref dynamic_arrays/darray.c</li>
+ *     <li>\ref dynamic_arrays/dptrarray.c</li>
+ *    </ul>
+ *  </li>
+ * </ul>
+ */
